0089e9e0f6810fc2ec232ea26da8e8af971deb92
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-27 21:12:13 +0530

    Update 01_Pointers.c

--- Practice/01_Pointers.c
+++ Practice/01_Pointers.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+void update(int *a,int *b) 
+{
+    // Complete this function  
+    int temp = *a;
+    *a = temp + *b;
+    *b = abs(*b - temp);  
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
 
